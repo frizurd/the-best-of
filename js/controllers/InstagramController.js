d71a8faae28c6a42d235ee9048bd015055fd063d
Frizky Kramer <frizkykramer@gmail.com>
2016-03-10 22:39:56 +0100

    instagram fix

--- js/controllers/InstagramController.js
+++ js/controllers/InstagramController.js
@@ -1,8 +1,9 @@
 app.controller('InstagramController', function($scope, instagram){
 
+  var search = 'baklava';
   $scope.imgs = [];
   instagram.fetchInsta(function(data) {
-      for(var i=0; i < 10; i++) {
+      for(var i=0; i < 1; i++) {
           $scope.imgs.push(data[i]) ;
       }
   });
--- js/services/instagram.js
+++ js/services/instagram.js
@@ -1,9 +1,9 @@
-app.factory('instagram', ['$http', function($http, $hash) {
+app.factory('instagram', ['$http', function($http) {
         return {
             fetchInsta: function(callback) {
 
                 var accessToken = "16384709.6ac06b4.49b97800d7fd4ac799a2c889f50f2587";
-                var url = "https://api.instagram.com/v1/tags/"+$hash+"/media/recent?client_id="+accessToken+"&access_token="+accessToken+"&callback=JSON_CALLBACK";
+                var url = "https://api.instagram.com/v1/tags/baklava/media/recent?client_id="+accessToken+"&access_token="+accessToken+"&callback=JSON_CALLBACK";
 
                 $http.jsonp(url).success(function(response) {
                     callback(response.data);
